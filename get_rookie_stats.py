8c6d1f9d356c0aefeb9c36befe9ec092edc28002
Mario Pérez <mario.perez17@alu.uclm.es>
2020-02-13 11:58:23 +0100

    Creación de scripts para obtener datos

---
+++ get_rookie_stats.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# NBA season we will be analyzing
+year = 2019
+# URL page we will scraping (see image above)
+url = "https://www.basketball-reference.com/leagues/NBA_{}_rookies-season-stats.html".format(year)
+# this is the HTML from the given URL
+html = urlopen(url)
+soup = BeautifulSoup(html,features="lxml")
+
+# use findALL() to get the column headers
+soup.findAll('tr', limit=2)
+# use getText()to extract the text we need into a list
+headers = [th.getText() for th in soup.findAll('tr', limit=2)[1].findAll('th')] # the first row is not the header
+# exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
+headers = headers[1:]
+
+# avoid the first header row
+rows = soup.findAll('tr')[1:]
+
+rookie_stats = [[td.getText() for td in rows[i].findAll('td')]
+            for i in range(len(rows))]
+
+rookie_stats_df = pd.DataFrame(rookie_stats, columns = headers)
+
+# Save stats in csv file
+rookie_stats_df.to_csv('rookie_stats.csv')
---
+++ get_team_stats.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# NBA season we will be analyzing
+year = 2019
+# URL page we will scraping (see image above)
+url = "https://www.basketball-reference.com/leagues/NBA_{}_ratings.html".format(year)
+# this is the HTML from the given URL
+html = urlopen(url)
+soup = BeautifulSoup(html,features="lxml")
+
+# use findALL() to get the column headers
+soup.findAll('tr', limit=2)
+# use getText()to extract the text we need into a list
+headers = [th.getText() for th in soup.findAll('tr', limit=2)[1].findAll('th')] # the first row is not the header
+# exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
+headers = headers[1:]
+
+# avoid the first header row
+rows = soup.findAll('tr')[1:]
+
+team_stats = [[td.getText() for td in rows[i].findAll('td')]
+            for i in range(len(rows))]
+
+team_stats_df = pd.DataFrame(team_stats, columns = headers)
+
+# Save stats in csv file
+team_stats_df.to_csv('team_stats.csv')
