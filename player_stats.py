18cf1514355bc3aeb287c34afb154855cfcef3a7
Mario Pérez <mario.perez17@alu.uclm.es>
2020-02-25 16:48:29 +0100

    Modelo regresion añadido

--- player_stats.py
+++ player_stats.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 def get_player_stats(year):
     # URL page we will scraping (see image above)
-    url = "https://www.basketball-reference.com/ \
-    leagues/NBA_{}_totals.html".format(year)
+    url = "https://www.basketball-reference.com/" \
+          "leagues/NBA_{}_totals.html".format(year)
     # this is the HTML from the given URL
     html = urlopen(url)
     soup = BeautifulSoup(html, features="html.parser")
@@ -14,8 +14,8 @@ def get_player_stats(year):
     # use findALL() to get the column headers
     soup.findAll('tr', limit=2)
     # use getText()to extract the text we need into a list
-    headers = [th.getText() for th in 
-        soup.findAll('tr', limit=2)[0].findAll('th')]
+    headers = [th.getText() for th in
+               soup.findAll('tr', limit=2)[0].findAll('th')]
     # exclude the first column as we will not need the 
     # ranking order from Basketball Reference for the analysis
     headers = headers[1:]
@@ -23,9 +23,9 @@ def get_player_stats(year):
     # avoid the first header row
     rows = soup.findAll('tr')[1:]
 
-    player_stats = [[td.getText() for td 
-        in rows[i].findAll('td')]
-        for i in range(len(rows))]
+    player_stats = [[td.getText() for td
+                     in rows[i].findAll('td')]
+                    for i in range(len(rows))]
 
     # Create DataFrame
     return pd.DataFrame(player_stats, columns=headers)
@@ -33,8 +33,8 @@ def get_player_stats(year):
 
 def get_rookie_stats(year):
     # URL page we will scraping (see image above)
-    url = "https://www.basketball-reference.com/ \
-    leagues/NBA_{}_rookies-season-stats.html".format(year)
+    url = "https://www.basketball-reference.com/" \
+          "leagues/NBA_{}_rookies-season-stats.html".format(year)
     # this is the HTML from the given URL
     html = urlopen(url)
     soup = BeautifulSoup(html, features="html.parser")
@@ -42,8 +42,8 @@ def get_rookie_stats(year):
     # use findALL() to get the column headers
     soup.findAll('tr', limit=2)
     # use getText()to extract the text we need into a list
-    headers = [th.getText() for th in 
-    soup.findAll('tr', limit=2)[1].findAll('th')]  
+    headers = [th.getText() for th in
+               soup.findAll('tr', limit=2)[1].findAll('th')]
     # the first row is not the header
     # exclude the first column as we will not need the 
     # ranking order from Basketball Reference for the analysis
@@ -52,8 +52,8 @@ def get_rookie_stats(year):
     # avoid the first header row
     rows = soup.findAll('tr')[1:]
 
-    rookie_stats = [[td.getText() for td 
-        in rows[i].findAll('td')]
-        for i in range(len(rows))]
+    rookie_stats = [[td.getText() for td
+                     in rows[i].findAll('td')]
+                    for i in range(len(rows))]
 
     return pd.DataFrame(rookie_stats, columns=headers)
